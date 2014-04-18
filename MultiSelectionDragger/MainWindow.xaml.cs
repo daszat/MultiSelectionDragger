c6e0fc753bf02541d2ff2355a06b2c214d40ea1a
David Schmitt <david@dasz.at>
2014-04-18 10:30:55 +0200

    Implement drag/drop
    Oh no, Multi Selection is broken

--- MultiSelectionDragger/MainWindow.xaml.cs
+++ MultiSelectionDragger/MainWindow.xaml.cs
@@ -23,8 +23,41 @@ namespace MultiSelectionDragger
         {
             InitializeComponent();
             DataContext = this;
+            List.MouseDown += List_MouseDown;
+            List.MouseMove += List_MouseMove;
         }
 
         public string[] Items { get { return new[] { "Foo", "Bar", "Baz", "Muh", "Blah" }; } }
+
+        #region Drag Drop Support
+
+        Point startPoint;
+
+        void List_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(null);
+        }
+
+        void List_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed
+                && (
+                    Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance
+                    || Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))
+            {
+                // Get the dragged ListViewItem
+                var items = string.Join(", ", List.SelectedItems.Cast<string>().ToArray());
+
+                // Initialize the drag & drop operation
+                DataObject dragData = new DataObject(DataFormats.UnicodeText, items);
+                DragDrop.DoDragDrop(List, dragData, DragDropEffects.Copy);
+            }
+        }
+
+        #endregion
     }
 }
