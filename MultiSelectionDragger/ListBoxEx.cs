34f0774a7137d34e380d8df937ea34836c406232
David Schmitt <david@dasz.at>
2014-04-18 10:36:32 +0200

    Implement dragging of multiple selected Items with minimal code

---
+++ MultiSelectionDragger/ListBoxEx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MultiSelectionDragger
+{
+    /// <summary>
+    /// This Extended ListBox can be used for dragging multiple selected items.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The default ListBox does not allow an implementation of the Windows Explorer's default drag/drop behaviour. This derived class repairs this by hacking around with custom ListBoxItems.
+    /// </para>
+    /// <para>
+    /// The same class can be easily adapted to ListView usage by replacing "Box" with "View".
+    /// </para>
+    /// </remarks>
+    public class ListBoxEx : ListBox
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new ListBoxItemEx();
+        }
+
+        class ListBoxItemEx : ListBoxItem
+        {
+            private bool _deferSelection = false;
+
+            protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+            {
+                if (e.ClickCount == 1 && IsSelected)
+                {
+                    // the user may start a drag by clicking into selected items
+                    // delay destroying the selection to the Up event
+                    _deferSelection = true;
+                }
+                else
+                {
+                    base.OnMouseLeftButtonDown(e);
+                }
+            }
+
+            protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+            {
+                if (_deferSelection)
+                {
+                    try
+                    {
+                        base.OnMouseLeftButtonDown(e);
+                    }
+                    finally
+                    {
+                        _deferSelection = false;
+                    }
+                }
+                base.OnMouseLeftButtonUp(e);
+            }
+
+            protected override void OnMouseLeave(MouseEventArgs e)
+            {
+                // abort deferred Down
+                _deferSelection = false;
+                base.OnMouseLeave(e);
+            }
+        }
+    }
+}
